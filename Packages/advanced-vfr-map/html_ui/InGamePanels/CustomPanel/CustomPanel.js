25b6138e3f7fd06e02034cd78e0f711a8a1aaf6f
Benjamin Mosnier <bm@kappuccino.org>
2020-12-19 16:15:37 +0100

    SimVar (get/set/subscribe) & Settings panel

--- Packages/advanced-vfr-map/html_ui/InGamePanels/CustomPanel/CustomPanel.js
+++ Packages/advanced-vfr-map/html_ui/InGamePanels/CustomPanel/CustomPanel.js
@@ -1,10 +1,3 @@
-// Set your name
-let userName = ''
-
-//////////////////////////////////////////////////////
-// Do not edit after
-/////////////////////////////////////////////////////
-
 const atcModelDic = {
 	"TT:ATCCOM.AC_MODEL_A20N.0.text": "A320neo",
 	"TT:ATCCOM.AC_MODEL A5.0.text": "A5",
@@ -33,43 +26,32 @@ const atcModelDic = {
 	"TT:ATCCOM.AC_MODEL_TBM9.0.text": "TBM 930"
 }
 
-const debug = false
-const version = "0.2.0"
-const sessionID = debug ? 1234 : Date.now()
-const groupName = 'global'
-
+const debug = true
+const version = '0.3.0'
+const groupName = debug ? 'dev' : 'global'
 const localIp = '192.168.1.20'
-if(debug) userName = 'crazyDev'
 
-const iframePath = `/map/${groupName}?ingame&v=${version}&session=${sessionID}`
 const iFrameServer = debug ? `http://${localIp}:8080` : 'https://flightsim.cloud'
-const iframeUrl = iFrameServer + iframePath
-
-const telemetryPath = '/d/ws/telemetryIn'
-const telemetryServer = debug ? `ws://${localIp}:1816` : 'wss://flightsim.cloud'
-const telemetryUrl = telemetryServer + telemetryPath
-
-const linkerPath = '/d/ws/linker/p' + sessionID
 const linkerServer = debug ? `ws://${localIp}:1816` : 'wss://flightsim.cloud'
-const linkerUrl = linkerServer + linkerPath
+const telemetryServer = debug ? `ws://${localIp}:1816` : 'wss://flightsim.cloud'
 
-let senderID = userName
-if(senderID === ''){
-	senderID = Math.random().toString(36).substring(2, 10)
-}else
-if(senderID.length > 10){
-	senderID = senderID.substring(0, 9)
-}
-
-/*let IngamePanelCustomPanelLoaded = false
-document.addEventListener('beforeunload', function(){
+let IngamePanelCustomPanelLoaded = false
+document.addEventListener('beforeunload', () => {
 	IngamePanelCustomPanelLoaded = false
-}, false)*/
+}, false)
 
 class IngamePanelCustomPanel extends HTMLElement {
 	constructor(){
 		super()
+
+		this.view = ''
 		this.planeHidden = false
+
+		this.sessionID = debug ? 123 : Date.now()
+		this.key = debug ? '456' : Math.random().toString(36).substring(2, 10)
+		this.randomName = Math.random().toString(36).substring(2, 10)
+
+		this.user = {}
 	}
 
 	connectedCallback(){
@@ -77,33 +59,149 @@ class IngamePanelCustomPanel extends HTMLElement {
 
 		this.m_toggleHidden = this.querySelector("#ToggleHidden")
 		if(this.m_toggleHidden){
+			this.m_toggleHidden.toggled = false
 			this.m_toggleHidden.addEventListener("OnValidate", this.toggleHidden.bind(this))
 		}
 
+		this.m_toggleSettings = this.querySelector("#ToggleSettings")
+		if(this.m_toggleSettings){
+			this.m_toggleSettings.toggled = false
+			this.m_toggleSettings.addEventListener("OnValidate", this.toggleSettings.bind(this))
+		}
+
+		// Views
+		this.viewUserSettings = this.querySelector('#user-settings')
+		this.iframe = document.querySelector("#CustomPanelIframe")
+
+		// Settings
+		this.i_userName = this.querySelector('input#user-name')
+		this.b_userSave = this.querySelector('#user-save')
+		if(this.b_userSave) this.b_userSave.addEventListener("OnValidate", this.saveUser.bind(this))
+
+		const debugDiv = this.querySelector('#debug')
+		if(debug && debugDiv) debugDiv.innerHTML = `key:${this.key} sessionID:${this.sessionID}`
+
+		this.updateUi()
+
+		// Reload the user from Storage when ready
+		RegisterViewListener('JS_LISTENER_DATASTORAGE', () => {
+			console.log('JS_LISTENER_DATASTORAGE: ready')
+
+			const user = this.getUser()
+			console.log(user)
+
+			if(!user){
+				console.log('No user in the store => open settings')
+				this.openSettings()
+				return
+			}
+
+			// Save globally
+			console.log('Save the user globally')
+			this.user = user
+
+			// Open the map (the user exists)
+			console.log('User exists, load the map')
+			this.setView('map')
+		})
+
+		// Start sockets
 		this.telemetryConnection()
-	//this.linkerConnection()
+		this.linkerConnection()
+	}
+
+	getUser(){
+		const user = GetStoredData('adv_vfr_user')
+		console.log('Raw store user')
+		console.log(user)
+
+		if(user) return JSON.parse(user)
+
+		console.log('Pas de user dans le store')
+
+		return {
+			name: ''
+		}
+	}
+
+	saveUser(){
+		const user = {
+			name: this.i_userName.value
+		}
+
+		const value = JSON.stringify(user)
+		SetStoredData('adv_vfr_user', value)
 	}
 
 	toggleHidden(){
 		this.planeHidden = this.m_toggleHidden.toggled
 	}
 
-	openiFrame(){
-		console.log('Open iFrame', iframeUrl)
-		let iframe = document.querySelector("#CustomPanelIframe")
-		if(iframe) iframe.src = iframeUrl
+	toggleSettings(){
+		if(this.m_toggleSettings.toggled) return this.setView('settings')
+		this.setView('map')
 	}
 
-	/***
-	 * Telemetry
-	 * ---------
+	setView(next){
+		this.view = next
+		this.updateUi()
+	}
+
+	updateUi(){
+		if(this.view === 'settings'){
+			this.openSettings()
+			this.closeiFrame()
+		}else
+		if(this.view === 'map'){
+			this.closeSettings()
+			this.openiFrame()
+		}
+	}
+
+	// Helpers
+
+	openSettings(){
+		this.viewUserSettings.style.display = 'block'
+		this.m_toggleSettings.toggled = true
+
+		this.i_userName.value = this.user.name || ''
+	}
+
+	closeSettings(){
+		this.viewUserSettings.style.display = 'none'
+		this.m_toggleSettings.toggled = false
+	}
+
+	openiFrame(){
+		if(!this.iframe) return
+
+		const url =  iFrameServer + `/map/${groupName}?ingame&__v=${version}&session=${this.sessionID}&key=${this.key}`
+		console.log('Open iFrame', url)
+
+		this.iframe.src = url
+		this.iframe.style.display = 'block'
+	}
+
+	closeiFrame(){
+		if(!this.iframe) return
+		console.log('Close iFrame')
+
+		this.iframe.src = ''
+		this.iframe.style.display = 'none'
+	}
+
+	/**
+	 * # Telemetry
+	 *
 	 * La telemetry envois la position de l'avion au server via un WebSocket `telemetryUrl`
 	 * on ne fait que publier sur ce canal de communication
 	 */
 
 	telemetryConnection(){
-		console.log('Connecting to Telemetry socket...', telemetryUrl)
-		this.telemetry = new WebSocket(telemetryUrl)
+		const url = telemetryServer + '/d/ws/telemetryIn'
+
+		console.log('Connecting to Telemetry socket...', url)
+		this.telemetry = new WebSocket(url)
 
 		this.telemetry.onopen = this.telemetryOnOpen.bind(this)
 		this.telemetry.onerror = this.telemetryOnError.bind(this)
@@ -128,7 +226,6 @@ class IngamePanelCustomPanel extends HTMLElement {
 	}
 
 	updatePos(){
-		console.log('Update pos')
 
 		let atcModel = SimVar.GetSimVarValue("ATC MODEL", "string", "FMC")
 		if(atcModelDic[atcModel]) atcModel = atcModelDic[atcModel]
@@ -153,15 +250,18 @@ class IngamePanelCustomPanel extends HTMLElement {
 		let vspeed = SimVar.GetSimVarValue("VERTICAL SPEED", "Feet per second")
 		if(vspeed) vspeed = vspeed.toFixed(2)
 
+		let userName = this.user && this.user.name ? this.user.name : this.randomName
+		userName = userName.substring(0, 9)
+
 		const data = {
 			hidden: this.planeHidden,
-			user: senderID,
+			user: userName,
 			group: groupName,
 			lng: lng,
 			lat,
 			alt,
 			hdg,
-			uuid: sessionID,
+			uuid: this.sessionID,
 			atcModel,
 			atcId,
 			atcAirline,
@@ -170,7 +270,7 @@ class IngamePanelCustomPanel extends HTMLElement {
 			vspeed
 		}
 
-		console.log(data)
+		//console.log('Update pos', data)
 
 		this.telemetry.send(JSON.stringify(data))
 
@@ -185,8 +285,10 @@ class IngamePanelCustomPanel extends HTMLElement {
 	 */
 
 	linkerConnection(){
-		console.log('Connecting to Linker socket...', linkerUrl)
-		this.linker = new WebSocket(linkerUrl)
+		const url = linkerServer + '/d/ws/linker/p' + this.key
+		console.log('Connecting to Linker socket...', url)
+
+		this.linker = new WebSocket(url)
 
 		this.linker.onopen = this.linkerOnOpen.bind(this)
 		this.linker.onerror = this.linkerOnError.bind(this)
@@ -209,14 +311,13 @@ class IngamePanelCustomPanel extends HTMLElement {
 	}
 
 	linkerOnMessage(msg){
-		console.log(msg.data)
-
 		if(msg.data === 'coucou') return
+
+		/*console.log('Raw MSG')
+		console.log(msg.data)*/
 
 		try {
 			const res = JSON.parse(msg.data)
-			console.log(res)
-
 			this.handleMsg(res)
 		} catch(err) {
 			console.log('Error decoding JSON')
@@ -225,7 +326,73 @@ class IngamePanelCustomPanel extends HTMLElement {
 	}
 
 	handleMsg(msg){
-		if(msg.type === 'simVarSubscribe') this.simVarSubscribe(msg.payload)
+		console.log('Object MSG')
+		console.log(msg)
+
+		if(msg.type === 'getSimVar') this.getSimVar(msg.id, msg.payload)
+		if(msg.type === 'setSimVar') this.setSimVar(msg.id, msg.payload)
+
+		/*
+		if(msg.type === 'simVarSubscribe') this.simVarSubscribe(msg.payload)*/
+	}
+
+	getSimVar(id, vars){
+		console.log('getSimVar()')
+		/*console.log('id')
+		console.log(id)
+		console.log('vars')
+		console.log(vars)*/
+
+		const values = vars.map(simvar => {
+			const v = SimVar.GetSimVarValue(simvar.name, simvar.unit, simvar.source)
+			return {name: simvar.name, value: v}
+		})
+
+		const msg = {
+			id,
+			type: 'getSimVar',
+			payload: values
+		}
+
+		console.log('SEND OBJ', msg)
+		console.log(msg)
+
+		const msgJson = JSON.stringify(msg)
+		this.linker.send(msgJson)
+	}
+
+	setSimVar(id, vars){
+		console.log('setSimVar()')
+		console.log('id')
+		console.log(id)
+
+		console.log('vars')
+		console.log(vars)
+
+		// SetSimVarValue(name, unit, value, dataSource = "")
+
+		vars.forEach(simvar => {
+			const v = SimVar.SetSimVarValue(simvar.name, simvar.unit, simvar.value, simvar.source)
+			return {name: simvar.name, value: v}
+		})
+
+/*
+		const values = vars.map(simvar => {
+
+		})
+
+		const msg = {
+			id,
+			type: 'getSimVar',
+			payload: values
+		}
+
+		console.log('SEND OBJ', msg)
+		console.log(msg)
+
+		const msgJson = JSON.stringify(msg)
+		this.linker.send(msgJson)
+*/
 	}
 
 	simVarSubscribe(conf){
